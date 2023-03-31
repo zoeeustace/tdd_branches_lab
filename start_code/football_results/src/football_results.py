e826b98ef27279c674dd5ced75c566fa262e7b1e
Zoe Eustace <zoeeustace@zoes-air.home>
2023-03-31 11:10:17 +0100

    football results first pass

--- start_code/football_results/src/football_results.py
+++ start_code/football_results/src/football_results.py
@@ -1,6 +1,12 @@
 def get_result(final_score):
-    pass
+    if final_score["Home score"] > final_score["Away score"]:
+        return "Home win"
+    elif final_score["Home score"] < final_score["Away score"]:
+        return "Away win"
+    elif final_score["Home score"] == final_score["Away score"]:
+        return "Draw"
 
 def get_results(final_scores):
-    pass
+    results = [get_result(score) for score in final_scores]
+    return results
     # (You could try and use a list comprehension for this)
--- start_code/football_results/tests/football_results_test.py
+++ start_code/football_results/tests/football_results_test.py
@@ -2,14 +2,45 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
     # Test we get the right result string for a final score dictionary representing -
-
+    def setUp(self):
+        self.final_score =  [
+        {
+            "Home score": 3,
+            "Away score": 1
+        },
+        {
+            "Home score": 1,
+            "Away score": 3
+        },
+        {
+            "Home score": 3,
+            "Away score": 3
+        },
+        ]
         # Home win
+    def test_can_get_a_home_win(self):
+        expected_value = "Home win"
+        actual_value = get_result(self.final_score[0])
+        self.assertEqual(expected_value, actual_value)
         # Away win
+
+    def test_can_get_an_away_win(self):
+        expected_value = "Away win"
+        actual_value = get_result(self.final_score[1])
+        self.assertEqual(expected_value, actual_value)
         # Draw
+    
+    def test_can_get_a_draw(self):
+        expected_value = "Draw"
+        actual_value = get_result(self.final_score[2])
+        self.assertEqual(expected_value, actual_value)
 
     # Test we get right list of result strings for a list of final score dictionaries. 
+    def test_can_get_list_of_results(self):
+        expected_value = ["Home win", "Away win", "Draw"]
+        actual_value = get_results(self.final_score)
+        self.assertEqual(expected_value, actual_value)
 
 
 if __name__ == "__main__":
